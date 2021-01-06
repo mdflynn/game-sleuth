89bf47727d7c632e127d01b2a60b2879b57d2f73
mdflynn <mdflynn34@outlook.com>
2021-01-05 20:16:18 -0700

    Set state with api data

--- src/SearchDisplay/SearchDisplay.tsx
+++ src/SearchDisplay/SearchDisplay.tsx
@@ -1,26 +1,52 @@
-import React, { Component } from 'react';
-import './SearchDisplay.scss';
+import React, { Component } from "react";
+import "./SearchDisplay.scss";
+import MyBoardGame from "../interfaces/MyBoardGame.interface";
+import { fetchSearchResults } from "../APIcalls";
 
-type MyProps = {
+interface MyProps {
   searchData: string;
-};
+}
 
-type MyState = {
-  boardGames: { name: string }[];
-};
-
-class SearchDisplay extends Component<MyProps, MyState> {
-  // constructor(props: IProps) {
-  //   super(props);
-  state: MyState = {
-    boardGames: [{ name: 'thing' }],
+class SearchDisplay extends Component<MyProps, MyBoardGame> {
+  state: MyBoardGame = {
+    boardGames: [
+      // {
+      //   id: "sweet string",
+      //   name: "sweet string",
+      //   min_players: 0,
+      //   max_players: 0,
+      //   min_age: 0,
+      //   image_url: "sweet string",
+      //   description: "sweet string",
+      //   price: "sweet string",
+      //   url: "sweet string",
+      //   primary_publisher: { name: "sweet string" },
+      //   mechanics: [{ id: "sweet string" }],
+      //   average_user_rating: 0,
+      //   num_user_ratings: 0,
+      //   rank: 0,
+      //   trending_rank: 0,
+      //   rules_url: "sweet string",
+      // },
+    ]
   };
+
+  componentDidMount() {
+    fetchSearchResults()
+      .then(data => {
+        this.setState({boardGames: data.games})
+        console.log(this.state.boardGames)
+        
+      })
+  }
+
+  functionToMapDisplayFetchData() {}
 
   render() {
     return (
       <h1>
         {this.props.searchData}
-        {this.state.boardGames[0].name}
+        {this.state.boardGames[0] && <h1>{this.state.boardGames[0].name}</h1>}
       </h1>
     );
   }
