a56f13f42f0bbe1fd3da29de2b73016811f1bbc5
Bryce Mara Jarrett <brycej98@gmail.com>
2021-01-09 15:53:18 -0600

    Add MainPage unit tests

--- src/MainPage/MainPage.test.tsx
+++ src/MainPage/MainPage.test.tsx
@@ -6,51 +6,81 @@ import { Router, MemoryRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history'
 jest.mock('../APIcalls')
 
-const expectedReturn = {
-  games: [
-    {
-      id: "j8LdPFmePE",
-      name: "7 Wonders Duel",
-      min_players: 2,
-      max_players: 2,
-      min_age: 10,
-      image_url:
-        "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559255812092-51S3zQnsPBL.jpg",
-      price: "24.97",
-    },
-  ],
-};
+// const expectedReturn = {
+//   games: [
+//     {
+//       id: "j8LdPFmePE",
+//       name: "7 Wonders Duel",
+//       min_players: 2,
+//       max_players: 2,
+//       min_age: 10,
+//       image_url:
+//         "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559255812092-51S3zQnsPBL.jpg",
+//       price: "24.97",
+//     },
+//   ],
+// };
 
 describe("App", () => {
-  it("should render correctly", async () => {
+  it("should render search form link", async () => {
     render(
       <MemoryRouter>
         <MainPage />
       </MemoryRouter>
     );
+
     const searchForm = screen.getByText("Search Form");
-    const fourPlayerGames = screen.getByText("4 Player Games")
+    
     expect(searchForm).toBeInTheDocument();
+    
+  });
+
+  it("should render search trending games link", async () => {
+    render(
+      <MemoryRouter>
+        <MainPage />
+      </MemoryRouter>
+    );
+    
+    const trendingGames = screen.getByText("Trending Games")
+    
+    expect(trendingGames).toBeInTheDocument();
+  });
+
+  it("should render search top 10 games link", async () => {
+    render(
+      <MemoryRouter>
+        <MainPage />
+      </MemoryRouter>
+    );
+    
+    const topTenGames = screen.getByText("Top 10 Games")
+    
+    expect(topTenGames).toBeInTheDocument();
+  });
+
+  it("should render search 4 player games link", async () => {
+    render(
+      <MemoryRouter>
+        <MainPage />
+      </MemoryRouter>
+    );
+    
+    const fourPlayerGames = screen.getByText("4 Player Games")
+    
     expect(fourPlayerGames).toBeInTheDocument();
   });
 
-  it("should be able to click search form link", async ()  => {
+  it("should render search 2 player games link", async () => {
     render(
       <MemoryRouter>
         <MainPage />
       </MemoryRouter>
     );
-    const searchForm =  await waitFor(() => screen.getByText("Search Form"))
-
-    fireEvent.click(searchForm)
-
-    // screen.debug()
-
-    const searchFormHeader = await waitFor(() => screen.queryByText("Search for Games!"))
-
-    expect(searchFormHeader).toBeInTheDocument()
-
-  })
-
+    
+    const twoPlayerGames = screen.getByText("2 Player Games")
+    
+    expect(twoPlayerGames).toBeInTheDocument();
+  });
 
 })
