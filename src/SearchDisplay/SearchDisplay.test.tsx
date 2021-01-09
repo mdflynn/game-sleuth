ce0a418e2125496b86e1eeed091d740cfc6aacb7
Bryce Mara Jarrett <brycej98@gmail.com>
2021-01-09 16:03:17 -0600

    Add unit tests to SearchDisplay

--- src/SearchDisplay/SearchDisplay.test.tsx
+++ src/SearchDisplay/SearchDisplay.test.tsx
@@ -18,45 +18,60 @@ const expectedReturn = {
         "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559255812092-51S3zQnsPBL.jpg",
       price: "24.97",
     },
+    {
+      id: "h8LdAFmeP0",
+      name: "Patchwork",
+      min_players: 2,
+      max_players: 2,
+      min_age: 4,
+      image_url:
+        "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559255812092-51S3zQnsPBL.jpg",
+      price: "30.97",
+    }
   ],
 };
 
+beforeEach(() => {
+  const mockedFetchCall = fetchSearchResults as jest.Mock<any>;
+  mockedFetchCall.mockResolvedValue(expectedReturn);
+
+})
+
 describe("SearchDisplay", () => {
+
   it("should render correctly", async () => {
-    const mockedFetchCall = fetchSearchResults as jest.Mock<any>;
-    mockedFetchCall.mockResolvedValue(expectedReturn);
     render(
       <MemoryRouter>
         <SearchDisplay />
       </MemoryRouter>
     );
+
     const header = await waitFor(() => screen.getByText("Search Results"));
+
     expect(header).toBeInTheDocument();
   });
 
-  it("should render correctly", async () => {
-    const mockedFetchCall = fetchSearchResults as jest.Mock<any>;
-    mockedFetchCall.mockResolvedValue(expectedReturn);
+  it("should render a movie", async () => {
     render(
       <MemoryRouter>
         <SearchDisplay />
       </MemoryRouter>
     );
+
     const gameTitle = await waitFor(() => screen.getByText("7 Wonders Duel"));
+
     expect(gameTitle).toBeInTheDocument();
   });
 
-  it("should be able to click on game & be redirected to solo view", async () => {
-    const mockedFetchCall = fetchSearchResults as jest.Mock<any>;
-    mockedFetchCall.mockResolvedValue(expectedReturn);
+  it("should render a different movie", async () => {
     render(
       <MemoryRouter>
         <SearchDisplay />
       </MemoryRouter>
     );
-      // MOCK FUNCTION CALLED THE SOLO GAME PREVIEW
-    
-    
-    
+
+    const gameTitle = await waitFor(() => screen.getByText("Patchwork"));
+
+    expect(gameTitle).toBeInTheDocument();
   });
 });
