bd55d217af4d8960182f2af025f95ff935c454f0
Bryce Mara Jarrett <brycej98@gmail.com>
2021-01-10 15:31:14 -0600

    Fix MainPage design

--- src/MainPage/MainPage.tsx
+++ src/MainPage/MainPage.tsx
@@ -4,37 +4,39 @@ import { Link } from "react-router-dom";
 
 const MainPage: React.FC = () => {
   return (
-    <ul>
-      <Link className="remove-link" to="/form">
-        <li className="search-form">
-          <h1>Search Form</h1>
-        </li>
-      </Link>
+    <main className="container">
+      <ul>
+        <Link className="remove-link" to="/form">
+          <li className="search-form">
+            <h3 className="title">Search Form</h3>
+          </li>
+        </Link>
 
-      <Link className="remove-link" to="/trending">
-        <li className="search-trending" data-value="trending">
-          Trending Games
-        </li>
-      </Link>
+        <Link className="remove-link" to="/trending">
+          <li className="search-trending" data-value="trending">
+            <h3 className="title">Trending Games</h3>
+          </li>
+        </Link>
 
-      <Link className="remove-link" to="/top">
-        <li className="search-top" data-value="top-10">
-          Top 10 Games
-        </li>
-      </Link>
+        <Link className="remove-link" to="/top">
+          <li className="search-top" data-value="top-10">
+            <h3 className="title">Top 10 Games</h3>
+          </li>
+        </Link>
 
-      <Link className="remove-link" to="/max_player=2">
-        <li className="search-two-player" data-value="max_players=2">
-          2 Player Games
-        </li>
-      </Link>
+        <Link className="remove-link" to="/max_player=2">
+          <li className="search-two-player" data-value="max_players=2">
+            <h3 className="title">2 Player Games</h3>
+          </li>
+        </Link>
 
-      <Link className="remove-link" to="/max_players=4">
-        <li className="search-four-player" data-value="max_player=4">
-          4 Player Games
-        </li>
-      </Link>
-    </ul>
+        <Link className="remove-link" to="/max_players=4">
+          <li className="search-four-player" data-value="max_player=4">
+            <h3 className="title">4 Player Games</h3>
+          </li>
+        </Link>
+      </ul>
+    </main>
   );
 };
 
