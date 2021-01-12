ea300c6474e194520c54c12c71b397e1a82b5eb0
Bryce Mara Jarrett <brycej98@gmail.com>
2021-01-12 11:21:44 -0600

    Add corrections to testing suites

--- src/Nav/Nav.test.tsx
+++ src/Nav/Nav.test.tsx
@@ -1,22 +1,19 @@
 import React from "react";
-import {
-  fireEvent,
-  render,
-  screen
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { MemoryRouter } from "react-router-dom";
 
 import Nav from "./Nav";
-import userEvent from "@testing-library/user-event";
 
-describe('Nav', () => {
-    it('should render the Nav component', () => {
-        render(
-            <MemoryRouter>
-              <Nav />
-            </MemoryRouter>
-        )
-        expect(screen.getByRole('link', { name: /game slueth/i })).toBeInTheDocument();
-    })
-})
+describe("Nav", () => {
+  it("should render the Nav component", () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole("link", { name: /game sleuth logo sleuth/i })
+    ).toBeInTheDocument();
+  });
+});
